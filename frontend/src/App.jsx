389101bf98f8ec4ac3fc6d44839ea9be4f8fff94
Tharindu Lakmal <tharindulakmal.k7@gmail.com>
2024-10-05 01:23:20 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './page/Home/Home'
+import Work from './page/Work/Work'
+import About from './page/About/About'
+import Contact from './page/Contact/Contact'
+
+function App() {
+  
+
+  return (
+    <div className='app'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/work' element={<Work />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/page/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.css"
+import Hero from '../../components/Home/Hero/Hero'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <Hero />
+    </div>
+  )
+}
+
+export default Home
