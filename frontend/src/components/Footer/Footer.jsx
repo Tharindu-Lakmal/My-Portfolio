07e6487cfd69ed88564b34bc44541ea33d5fe0c8
Tharindu Lakmal <tharindulakmal.k7@gmail.com>
2024-10-08 19:36:05 +0530

    styles added

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
 
             <div className="footer-content">
                 <h1 className="footer-header">
-                  Let's build somthing
+                  <span>Let's build something</span>
                   <span>Amazing!</span>
                 </h1>
             </div>
@@ -31,6 +31,8 @@ const Footer = () => {
 
             <div className="social-links">
               <div className="social-links-inner">
+                {/* <iframe src="https://lottie.host/embed/647cd8eb-d93f-486d-bdba-d1c95a61baca/mK2YMCZilm.json"></iframe> */}
+                <iframe src="https://lottie.host/embed/fff8f992-0dd2-479c-92ac-0f69f1dfa53d/rXfmeRCLiA.json"></iframe>
                 <p className="social-link">LinkdIn</p>
                 <p className="social-link">GitHub</p>
               </div>
